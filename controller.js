61ae75373020e364f82f60ef5ccefb18651b0fe3
Jinonda Juan <andrejuan103@gmail.com>
2022-06-07 20:33:39 +0700

    success showall playlist

--- controller.js
+++ controller.js
@@ -1,8 +1,19 @@
 'use strict'
 
-var response = requeire('./res');
+var response = require('./res');
 var connection = require('./koneksi');
 
 exports.index = function(req,res){
-    response.ok("Application REST API Running")
+    response.ok("Application REST API Running",res)
+};
+
+//menampilkan semua data mahasiswa
+exports.showallsonglist = function(req,res) {
+    connection.query('SELECT * FROM data_songlist', function(error, rows, fields){
+        if(error){
+            connection.log(error);
+        }else {
+            response.ok(rows, res)
+        }
+    });
 };
